edfb784c6fc5b8bb66378df6714a1e4a763d8666
Alpha <alphashuro@gmail.com>
2016-01-14 21:48:14 +0200

    configured jssor slider for clients

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 jQuery(function($) {'use strict';
 
+	$(document).ready(function() {
+		var jssor_1_options = {
+    $AutoPlay: true,
+    $Idle: 0,
+    $AutoPlaySteps: 4,
+    $SlideDuration: 1600,
+    $SlideEasing: $Jease$.$Linear,
+    $PauseOnHover: 4,
+    $SlideWidth: 200,
+    $Cols: 10
+  };
+  
+  var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+  
+  //responsive code begin
+  //you can remove responsive code if you don't want the slider scales while window resizing
+  function ScaleSlider() {
+      var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+      if (refSize) {
+          refSize = Math.min(refSize, 809);
+          jssor_1_slider.$ScaleWidth(refSize);
+      }
+      else {
+          window.setTimeout(ScaleSlider, 30);
+      }
+  }
+  ScaleSlider();
+  $(window).bind("load", ScaleSlider);
+  $(window).bind("resize", ScaleSlider);
+  $(window).bind("orientationchange", ScaleSlider);
+  //responsive code end
+	});
+
 	// Navigation Scroll
 	$(window).scroll(function(event) {
 		Scroll();
